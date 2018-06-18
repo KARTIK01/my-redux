44ba3e201d248eede2251387f52b1b1b32400662
KARTIK AGARWAL <kartik.agarwal@practo.com>
2018-06-18 23:49:03 +0530

    before

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
+const store = createStore(rootReducer);
+
+ReactDOM.render( <Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
